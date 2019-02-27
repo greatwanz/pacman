4e7dcc3a5af04038de0cc3eacf52f5ace7b277af
greatwanz <tinywanz@gmail.com>
2019-02-27 16:24:14 +0800

    Basic main menu, UI and 3D GameObjects added

---
+++ pacman/Assets/Script/FlashText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Flashes a text on screen. Requires the GameObject to have a Text component
+/// </summary>
+public class FlashText : MonoBehaviour
+{
+    public float waitTime;
+    public Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Flash());
+    }
+
+    IEnumerator Flash()
+    {
+        while (true)
+        {
+            text.gameObject.SetActive(true);
+            yield return new WaitForSeconds(waitTime);
+            text.gameObject.SetActive(false);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
---
+++ pacman/Assets/Script/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Reflection;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    [AssertNotNull]public GameObject titleScreenUI;
+    [AssertNotNull]public GameObject gameScreenUI;
+    [AssertNotNull]public GameObject gameScreenObjects;
+
+    void Awake()
+    {
+        if (Application.isEditor)
+        {
+            AttributeAssert(AssertFieldNotNull);
+        }
+
+        titleScreenUI.SetActive(true);
+        gameScreenUI.SetActive(false);
+        gameScreenObjects.SetActive(false);
+    }
+
+    void Start()
+    {
+        StartCoroutine(GameLoop());	
+    }
+
+    IEnumerator GameLoop()
+    {
+        yield return StartGame();
+        yield return PlayingGame();
+        yield return EndGame();
+    }
+
+    IEnumerator StartGame()
+    {
+        //Wait until space is pressed
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        titleScreenUI.SetActive(false);
+    }
+
+    IEnumerator PlayingGame()
+    {
+        gameScreenUI.SetActive(true);
+        gameScreenObjects.SetActive(true);
+        yield return null;
+    }
+
+    IEnumerator EndGame()
+    {
+        yield return null;
+    }
+        
+    // Checks for the validity of a particular attribute
+    void AttributeAssert(params Action<MonoBehaviour,Type,FieldInfo[]>[] assertArray)
+    {
+        MonoBehaviour[] sceneActive = FindObjectsOfType<MonoBehaviour>();
+        foreach (MonoBehaviour script in sceneActive)
+        {
+            var scriptType = script.GetType();
+            var fields = scriptType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (Action<MonoBehaviour,Type,FieldInfo[]> assert in assertArray)
+            {
+                assert(script, scriptType, fields);
+            }
+        }
+    }
+
+    //Checks whether a particular field is unassigned or null
+    void AssertFieldNotNull(MonoBehaviour script, Type scriptType, FieldInfo[] fields)
+    {
+        foreach (var field in fields)
+        {
+            var assertNotNulls = field.GetCustomAttributes(typeof(AssertNotNull), true);
+            if (assertNotNulls.Length < 1)
+                continue;
+
+            var value = field.GetValue(script);
+            if (value == null || (value is UnityEngine.Object && (UnityEngine.Object)value == null))
+            {
+                Debug.LogErrorFormat(script, "Script '{0}', field '{1}' is unassigned.", scriptType.Name, field.Name);
+                break;
+            }
+        }
+    }
+}
